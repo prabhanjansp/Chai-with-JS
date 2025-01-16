a2d63b9b76f02be3ce12cc4283952939821d28a3
prabhanjansp <puranikchetan97@gmail.com>
2025-01-16 16:05:44 +0530

    compare data type

---
+++ comparision.js
@@ -0,0 +1,18 @@
+// console.log("2">1);
+// console.log("02">1);
+// typescript won't allow to compare two different datatype
+console.log(null>0) //false
+console.log(null==0) //false
+console.log(null>= 0) // true
+
+
+
+
+console.log(undefined==0);//false
+console.log(undefined>0);//false
+console.log(undefined<0);//false
+
+//  in javascript comparison and equality operator two different things
+// strict comparison
+// it won't convert it to  any data type
+// 
