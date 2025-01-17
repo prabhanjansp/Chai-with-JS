bab1c49d1f2fa44881eb0108e28c5dfb9afee9ba
prabhanjansp <puranikchetan97@gmail.com>
2025-01-17 12:25:45 +0530

    destructure in js

---
+++ ChapterTwo-Js-Core/destructure.js
@@ -0,0 +1,11 @@
+// object destructure
+
+const course={
+    name:"js",
+    price:999,
+    instructor:"prabhanjan"
+}
+const{instructor:courseInstructor}=course //way to access the object value where i can use it when ever i  want and also i can specify my own name
+// console.log(instructor)
+console.log(courseInstructor) //object destructuring
+//**************API***************** */
