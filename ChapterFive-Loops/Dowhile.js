1d9fcf42399aa18649d40813847ff4da8e453f1f
prabhanjansp <puranikchetan97@gmail.com>
2025-01-23 16:42:03 +0530

    while and do while loop

---
+++ ChapterFive-Loops/Dowhile.js
@@ -0,0 +1,17 @@
+// do while loop
+// do {
+    
+// } while (condition);
+
+let score=1
+do {
+    console.log(`first score is ${score}`);
+    score++;
+} while (score<=10);
+
+
+let score1=11  // will execute the loop atleaset once
+do {
+    console.log(`first score is ${score1}`);
+    score++;
+} while (score1<=10);
---
+++ ChapterFive-Loops/While.js
@@ -0,0 +1,21 @@
+// while loop
+
+// while (condition) {
+
+// }
+
+let i = 0;
+while (i <= 10) {
+  console.log(` value of index is ${i}`);
+  i = i + 2;
+}
+
+const myArray=["apple","banana","mango","orange","grapes"];
+let arr=0
+while (arr < myArray.length) {
+  console.log(myArray[arr]);
+    arr=arr+1;
+    // arr++
+}
+
+
